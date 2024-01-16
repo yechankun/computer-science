b57952851f0a5bb328d9f159dc2b1cded1ef5ceb
장예찬 <redniche@naver.com>
2024-01-16 19:07:40 +0900

    structure: 큐 테스트

--- src/큐/Queue테스트.java
+++ src/큐/Queue테스트.java
@@ -1,8 +1,35 @@
 package 큐;
 
-import java.util.LinkedList; 
-import java.util.Queue; 
+import 연결리스트.LinkedList;
 
 public class Queue테스트 {
     
+    public static void main(String[] args){
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue);
+
+        System.out.println(queue.poll());
+
+        System.out.println(queue);
+
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+
+        System.out.println(deque);
+        System.out.println(deque.pollLast());
+        System.out.println(deque);
+        System.out.println(deque.pollFirst());
+        System.out.println(deque);
+
+    }
 }
+
